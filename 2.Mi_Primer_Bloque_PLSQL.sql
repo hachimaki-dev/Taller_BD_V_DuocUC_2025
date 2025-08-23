3da41239c35d57861b4be884325a5425f0d5a636
HachiMakiDev <hachimakidev@gmail.com>
2025-08-22 20:18:57 -0400

    Avances de la clase del Viernes 22 de agosto del 2025

---
+++ 2.Mi_Primer_Bloque_PLSQL.sql
@@ -0,0 +1,18 @@
+DECLARE
+v_nombre usuarios.nombre%TYPE;
+v_saldo usuarios.saldo%TYPE;
+v_email usuarios.email%TYPE;
+v_fecha_registro usuarios.fecha_registro%TYPE;
+
+BEGIN
+    SELECT nombre, saldo, email, fecha_registro
+    INTO v_nombre, v_saldo, v_email, v_fecha_registro
+    FROM usuarios
+    WHERE id_usuario = 2;
+
+    DBMS_OUTPUT.PUT_LINE('Nombre: ' || v_nombre);
+    DBMS_OUTPUT.PUT_LINE('Saldo: ' || v_saldo);
+    DBMS_OUTPUT.PUT_LINE('Email: ' || v_email);
+    DBMS_OUTPUT.PUT_LINE('Fecha registro: '|| v_fecha_registro);
+END;
+/
